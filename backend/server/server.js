e2faf8b12fdaec2aed55423faa61b0178bdce416
Berny Itoutou <berny-francois@cosavostra.com>
2023-03-09 15:20:31 +0100

    made some updates

--- backend/server/server.js
+++ backend/server/server.js
@@ -9,7 +9,7 @@ const dbConnection = require('./database/connection')
 dotEnv.config()
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 
 // Connect to the database-sample
 dbConnection()
